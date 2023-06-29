5e64d8955a151b930097f3037a8a61b3fc593e3b
Marc Bertens-Nguyen <marc.bertens@equensworldline.com>
2023-06-29 09:31:04 +0200

    Update the generic checkprocess plugin

--- sysinvest/monitor/checkprocess/worker.py
+++ sysinvest/monitor/checkprocess/worker.py
@@ -17,88 +17,56 @@
 #   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 #   Boston, MA 02110-1301 USA
 #
-import os
 import traceback
 from sysinvest.common.plugin import MonitorPlugin, PluginResult
 import sysinvest.common.plugin.constants as const
-import copy
 import psutil
-from sysinvest.common.checkpid import checkPidFilename
 import sysinvest.common.api as API
 
+
 class CheckProcess( MonitorPlugin ):
+    DEFAULT_TEMPLATE = """${message}
+    
+"""
     def execute( self ) -> None:
         super().execute()
         task_result = PluginResult( self )
         try:
             result = None
-            processName = self.Attributes.get( const.C_NAME )
-            listOfProcessObjects = []
-            processAttrs = self.Attributes.get( const.C_INFO, [ const.C_PID, const.C_NAME, const.C_CREATE_TIME ] )
-            commandline = self.Attributes.get( const.C_COMMANDLINE )
-            if commandline is not None:
-                if const.C_COMMANDLINE not in processAttrs:
-                    processAttrs.append( const.C_COMMANDLINE )
-
-            pidfile = self.Attributes.get( const.C_PID_FILE )
-            if pidfile is not None:
-                if os.path.exists( pidfile ):
-                    result, pid = checkPidFilename( pidfile )
-                    # now check if the PID exists
-                    if not result:
-                        task_result.update( False, f"a process with pid {pid} does not exist" )
-
-                else:
-                    task_result.update( False, f"PID filename {pidfile} doesn't exist" )
-
-            if isinstance( result, PluginResult ):
-                return result
-
-            thresholds = self.Attributes.get( const.C_THRESHOLDS )
-            if thresholds is not None:
-                for key in thresholds.keys():
-                    if key not in processAttrs:
-                        processAttrs.append( key )
+            executable = self.Attributes.get( 'executable' )
+            commandline = self.Attributes.get( 'commandline', [] )
+            if isinstance( commandline, str ):
+                commandline = commandline.split( ' ' )
 
             for proc in psutil.process_iter():
                 try:
-                    pinfo = proc.as_dict( processAttrs )
-                    # # Check if process name contains the given name string.
-                    if processName in pinfo[ const.C_NAME ] and commandline == pinfo[ const.C_COMMANDLINE ][ 1: ]:
-                        listOfProcessObjects.append( pinfo )
+                    cmdline = proc.cmdline()
+                    self.log.info( "Process {}".format( " ".join( cmdline ) ) )
+                    if len( cmdline ) > 0 and cmdline[0] == executable:
+                        count = 0
+                        for arg in commandline:
+                            if arg in cmdline[1:]:
+                                count += 1
 
-                except (psutil.NoSuchProcess, psutil.AccessDenied , psutil.ZombieProcess) :
-                    pass
-
-            if len( listOfProcessObjects ) == 0:
-                task_result.update( self, False, f"Process: {processName} doesn't exist" )
-
-            else:
-                if thresholds is None:
-                    task_result.update( self, True, f"Process: {processName} active", { 'processes': listOfProcessObjects } )
-
-                else:
-                    data = copy.copy(thresholds)
-                    for key in thresholds.keys():
-                        data[ key ] = 0
-
-                    for item in listOfProcessObjects:
-                        for key, value in thresholds.items():
-                            data[ key ] += item[ key ]
-
-                    result = None
-                    for tkey, tvalue in thresholds.items():
-                        if data[ tkey ] > tvalue:
-                            # Exceeding the threshold
-                            task_result.update( self, False,
-                                                   f"Process: {processName} active, but exceeds the {tkey} threshold {tvalue} with {data[ tkey ]}",
-                                                   { 'processes': listOfProcessObjects } )
+                        if count == len(commandline):
+                            result = ' '.join( commandline )
                             break
 
-                    if result is None:
-                        task_result.update( True, f"Process: {processName} active", { 'processes': listOfProcessObjects } )
+                except psutil.AccessDenied:
+                    pass
+
+                except Exception:
+                    raise
+
+            if result is None:
+                result = ' '.join(commandline)
+                task_result.update( False, f"Executable {executable} with command line '{result}' doesn't exist" )
+
+            else:
+                task_result.update( True, f"Executable {executable} with command line '{result}' exist" )
 
         except Exception as exc:
+            self.log.exception( "During lookup process" )
             task_result.update( False, str(exc), { const.C_EXCEPTION: exc, const.C_TRACEBACK: traceback.format_exc() } )
 
         API.QUEUE.put( task_result )
